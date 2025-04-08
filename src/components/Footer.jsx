ddcbc66bd5201de34d8b76c037ed584ee54e0af2
Izydor123 <164671009+Izydor123@users.noreply.github.com>
2025-04-08 14:08:59 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className="footer-section">
           <h4>Contate-nos</h4>
           <p>Email: <a href="mailto:geral@deliver2yougift.com">geral@deliver2yougift.com</a></p>
-          <p>Telefone: <a href="tel:00351912224405">+351 912 224 405</a></p>
-          <p>Morada: Estrada Principal da Rocha Nova, 1- Nascente Santo António dos Olivais, Coimbra 3020-249</p>
+          <p>Telefone: <a href="tel:00351239169651">+351 239 169 651</a></p>
+          <p>Morada: Rua da Quinta 27 A, 3040-823 Cernache</p>
           
           <div className="social-links">
             <a href="https://www.facebook.com/Deliver2youGift" target='_blank' aria-label="Facebook" className="social-icon">
